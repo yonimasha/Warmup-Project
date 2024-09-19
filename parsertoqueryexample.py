03b3892829e459a37493114fa8b2c06c79ea3dd6
cwodarz <112415123+cwodarz@users.noreply.github.com>
2024-09-19 11:51:38 -0400

    Add files via upload

---
+++ parsertoqueryexample.py
@@ -0,0 +1,9 @@
+import queryattempt
+query = "artist"
+operator = "=="
+info = "Travis Scott"
+listqueries = [query, operator, info]
+
+queryattempt.make_query(listqueries) # Assuming there’s a function named ‘main’ in script2.py
+
+
---
+++ queryattempt.py
@@ -0,0 +1,15 @@
+def make_query(listqueries):
+    #print(listqueries)
+    # check the length of the list to determine if it is a compound query 
+    #if len(listqueries) == 3: 
+        # the query is not compound 
+        # check the operator 
+        #operator = ""
+        #if listqueries[1] == "<":
+            # call func
+            #print("<")
+    for item in listqueries:
+        print(item)
+    
+
+#make_query(listqueries)
